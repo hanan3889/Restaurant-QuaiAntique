2afed4b3c63b63a4bd28243adb1929a22226f49b
hanan Rebaia <rebaia_hanan@yahoo.fr>
2023-03-13 15:30:40 +0100

    ✨add images fixtures with faker

--- src/DataFixtures/ImagesFixtures.php
+++ src/DataFixtures/ImagesFixtures.php
@@ -2,16 +2,26 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class ImagesFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create('fr_FR');
 
-        $manager->flush();
+        for($i = 1; $i <= 10; $i++ ){
+            $img = new Image();
+            $img->setTitle($faker->word);
+            $img->setDescription($faker->text(200));
+            $img->setPrice($faker->numberBetween(7, 30));
+            
+            $manager->persist($img);
+        }
+
+            $manager->flush();
     }
 }
